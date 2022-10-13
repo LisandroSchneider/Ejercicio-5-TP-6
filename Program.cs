686b074d2bd59d05caaf3d9d9f9e0dd052e59da8
lisandroschneider <lisandroschneider7@gmail.com>
2022-10-13 18:39:04 -0300

    subir archivo

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ejercicio_n_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int n1,n2;
+            string dato1,dato2;
+            
+
+        //solicitar dato
+
+            Console.WriteLine("digite el primer numero");
+            dato1 = Console.ReadLine();
+            n1 = Convert.ToInt32(dato1);
+
+            Console.WriteLine("digite el segundo numero");
+            dato2 = Console.ReadLine();
+            n2 = Convert.ToInt32(dato2);
+
+            //verificador
+            if(n1<n2)
+            {
+                Console.WriteLine("{0} es mayor que: {1}",n2,n1);
+            }
+            else
+            {
+                Console.WriteLine("{0} es mayor que: {1}",n1,n2);
+            }
+        }
+    }
+}
